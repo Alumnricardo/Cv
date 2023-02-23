cf6fb8a08924ddd513e0f1bb40e7608626619c70
Alumnricardo <richielcardo26@gmail.com>
2023-02-23 15:02:52 -0300

    reparando modo obscuro iconos

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,6 @@
 
+let obscuro = false;
+ console.log(obscuro);
 
 /*
 
@@ -7,17 +9,37 @@ document.addEventListener("click", function(){
   });
 */
   document.getElementById("boton").addEventListener("click", function(){
-    console.log("msj 001 bit");
+    
     alert("Seleccionó modo Obscuro");
+    
+    obscuro = !obscuro;
+  console.log(obscuro);
+
+  if (obscuro) {
+    document.getElementById("botonmodo").className = "bi bi-sun";
+    
     document.getElementById("barra").className = "navbar navbar-expand-lg navbar-dark bg-dark";
-    document.getElementById("boton").innerHTML = "\uF5A2";
     document.getElementById("boton").className = "btn btn-dark";
-
     const collection = document.getElementsByClassName("ventana2");
-for (let i = 0; i < collection.length; i++) {
-  collection[i].className = "ventana3";
-  collection[i++].className = "ventana3";
-}
-
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].className = "ventana3";
+    }
+    
+    
+    
+    
+ } else {
+    document.getElementById("botonmodo").className = "bi bi-moon";
+    document.getElementById("barra").className = "navbar navbar-expand-lg navbar-dark bg-primary";
+    document.getElementById("boton").className = "btn btn-primary";
+    const collection2 = document.getElementsByClassName("ventana3");
+    for (let i = 0; i < collection2.length; i++) {
+        collection2[i].className = "ventana2";
+    }
+ }
     
   });
+
+
+  
+
