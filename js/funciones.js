144590b693874fdcdc741270897bd6c4b2fc48e4
Alumnricardo <richielcardo26@gmail.com>
2023-02-26 17:34:04 -0300

    multipples cambios contenido de sitio

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,7 @@
 
 let obscuro = false;
  console.log(obscuro);
-
+ 
 /*
 
 document.addEventListener("click", function(){
@@ -13,33 +13,63 @@ document.addEventListener("click", function(){
 
 /* //////////////////////  FUNCION QUE CAMBIA CLASES PARA CREAR EL MODO OBSCURO ///////////////////////////////   */ 
   document.getElementById("boton").addEventListener("click", function(){
-    
-    
-    
-    obscuro = !obscuro;
+  obscuro = !obscuro;
   console.log(obscuro);
-
   if (obscuro) {
-    document.getElementById("botonmodo").className = "bi bi-sun";
-    
-    document.getElementById("barra").className = "navbar navbar-expand-lg navbar-dark bg-dark";
-    document.getElementById("boton").className = "btn btn-dark";
-    const collection = document.getElementsByClassName("ventana2");
-    for (let i = 0; i < collection.length; i++) {
+document.body.style.backgroundColor = "#282828";
+
+
+document.getElementById("v1").style.backgroundColor = "#282828";
+document.getElementById("v2").style.backgroundColor = "#282828";
+document.getElementById("v3").style.backgroundColor = "#282828";
+
+document.getElementById("t1").style.color = "white";
+document.getElementById("t2").style.color = "white";
+document.getElementById("t3").style.color = "white";
+
+
+document.getElementById("botonmodo").className = "bi bi-sun";
+document.getElementById("barra").className = "navbar navbar-expand-lg navbar-dark bg-dark";
+document.getElementById("boton").className = "btn btn-dark";
+document.getElementById("botoninfo").className = "btn btn-dark";
+const collection = document.getElementsByClassName("ventana2");
+
+   
+for (let i = 0; i < collection.length; i++) {
         collection[i].className = "ventana3";
     }
+    
+  
+    
     
     
     
     
  } else {
     document.getElementById("botonmodo").className = "bi bi-moon";
+    
+    document.body.style.backgroundColor = "white";
+
+    document.getElementById("v1").style.backgroundColor = "white";
+    document.getElementById("v2").style.backgroundColor = "white";
+    document.getElementById("v3").style.backgroundColor = "white";
+
+    document.getElementById("t1").style.color = "#236CC4";
+    document.getElementById("t2").style.color = "#236CC4";
+    document.getElementById("t3").style.color = "#236CC4";
+
     document.getElementById("barra").className = "navbar navbar-expand-lg navbar-dark bg-primary";
     document.getElementById("boton").className = "btn btn-primary";
+    document.getElementById("botoninfo").className = "btn btn-primary";
     const collection2 = document.getElementsByClassName("ventana3");
-    for (let i = 0; i < collection2.length; i++) {
+       for (let i = 0; i < collection2.length; i++) {
         collection2[i].className = "ventana2";
-    }
+         }
+    
+    
+
+
+    
  }
     
   });
